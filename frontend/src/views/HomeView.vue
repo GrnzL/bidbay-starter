823928880e29a657e4fe650916f7c3a0f12c4f01
Louis <louis.gernez@etu.univ-lyon1.fr>
2024-04-08 10:49:23 +0200

    Pour faire les tests

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -11,7 +11,7 @@ async function fetchProducts() {
   loading.value = true;
   error.value = false;
 
-  try{
+  try {
     const response = await fetch("http://localhost:3000/api/products");
     products.value = await response.json();
   } catch (e) {
@@ -22,22 +22,22 @@ async function fetchProducts() {
 }
 
 const filteredProducts = computed(() =>
-    products.value.filter((product) =>
-        product.name.toLowerCase().includes(filter.value.toLowerCase())
-    )
+  products.value.filter((product) =>
+    product.name.toLowerCase().includes(filter.value.toLowerCase()),
+  ),
 );
 
 const sortedProducts = computed(() =>
-    // eslint-disable-next-line vue/no-side-effects-in-computed-properties
-    filteredProducts.value.sort((a, b) => {
-      if (sorter.value === "name") {
-        return a.name.localeCompare(b.name);
-      } else if (sorter.value === "price") {
-        return a.originalPrice - b.originalPrice;
-      } else {
-        return 0;
-      }
-    })
+  // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+  filteredProducts.value.sort((a, b) => {
+    if (sorter.value === "name") {
+      return a.name.localeCompare(b.name);
+    } else if (sorter.value === "price") {
+      return a.originalPrice - b.originalPrice;
+    } else {
+      return 0;
+    }
+  }),
 );
 
 fetchProducts();
@@ -51,7 +51,6 @@ const getHighestBid = (product) => {
   });
   return highestBid.price;
 };
-
 </script>
 
 <template>
@@ -64,6 +63,7 @@ const getHighestBid = (product) => {
           <div class="input-group">
             <span class="input-group-text">Filtrage</span>
             <input
+              v-model="filter"
               type="text"
               class="form-control"
               placeholder="Filtrer par nom"
@@ -81,13 +81,13 @@ const getHighestBid = (product) => {
             aria-expanded="false"
             data-test-sorter
           >
-            Trier par nom
+            Trier par {{ sorter }}
           </button>
           <ul class="dropdown-menu dropdown-menu-end">
-            <li>
+            <li v-on:click="sorter='name'">
               <a class="dropdown-item" href="#"> Nom </a>
             </li>
-            <li>
+            <li v-on:click="sorter='price'">
               <a class="dropdown-item" href="#" data-test-sorter-price>
                 Prix
               </a>
@@ -103,14 +103,30 @@ const getHighestBid = (product) => {
       </div>
     </div>
 
-    <div class="alert alert-danger mt-4" role="alert" data-test-error v-if="error">
+    <div
+      class="alert alert-danger mt-4"
+      role="alert"
+      data-test-error
+      v-if="error"
+    >
       Une erreur est survenue lors du chargement des produits.
     </div>
     <div class="row">
-      <div class="col-md-4 mb-4" v-for="product in sortedProducts" :key="product.id" data-test-product>
+      <div
+        class="col-md-4 mb-4"
+        v-for="product in sortedProducts"
+        :key="product.id"
+        data-test-product
+      >
         <div class="card">
-          <RouterLink :to="{ name: 'Product', params: { productId: product.id } }">
-            <img :src="product.pictureUrl" class="card-img-top" :alt="product.name" />
+          <RouterLink
+            :to="{ name: 'Product', params: { productId: product.id } }"
+          >
+            <img
+              :src="product.pictureUrl"
+              class="card-img-top"
+              :alt="product.name"
+            />
           </RouterLink>
           <div class="card-body">
             <h5 class="card-title">
@@ -134,7 +150,8 @@ const getHighestBid = (product) => {
               </RouterLink>
             </p>
             <p class="card-text" data-test-product-date>
-              En cours jusqu'au {{ new Date(product.endDate).toLocaleDateString('fr-FR') }}
+              En cours jusqu'au
+              {{ new Date(product.endDate).toLocaleDateString("fr-FR") }}
             </p>
             <p class="card-text" data-test-product-price>
               Prix de départ : {{ product.originalPrice }} €
